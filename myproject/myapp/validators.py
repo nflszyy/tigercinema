d03b6ce3913ea03f6e34b08beb9301264f21017f
Iden <ikalemaj@princeton.edu>
2017-04-22 13:57:06 -0400

    changed validators.py

--- myproject/myapp/validators.py
+++ myproject/myapp/validators.py
@@ -1,9 +1,5 @@
-import magic
 from django.core.exceptions import ValidationError
 def validate_mime_type(value):
     supported_types=['video/mp4',]
-    with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
-        mime_type=m.id_buffer(value.file.read(1024))
-        value.file.seek(0)
-    if mime_type not in supported_types:
-        raise ValidationError(u'Unsupported file type.')
+    if value.content_type not in supported_types:
+    	raise ValidationError(u'Unsupported file type.')
