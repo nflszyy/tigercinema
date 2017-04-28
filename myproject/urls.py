b170f6144c711c8d89440f38db50d6eaaa70ab87
Haochen Li <haochenl@princeton.edu>
2017-04-28 15:08:13 -0400

    cas

--- myproject/urls.py
+++ myproject/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^myapp/', include('myproject.myapp.urls')),
     url(r'^$', RedirectView.as_view(url='/myapp/welcome/', permanent=False)),
-    url(r'^accounts/login/$', django_cas.views.login,name='login'),
-    url(r'^accounts/logout/$', django_cas.views.logout,name='logout'),
+    url(r'^accounts/login/$', 'django_cas.views.login',name='login'),
+    url(r'^accounts/logout/$', 'django_cas.views.logout',name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
