e4224e6680d3525d9d126bb132955971077382f7
Haochen Li <haochenl@princeton.edu>
2017-04-27 22:00:41 -0400

    cas

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from myproject.myapp import views
+import django_cas_ng.views
 
 urlpatterns = [
     url(r'^welcome/$', views.welcome, name='welcome'),
