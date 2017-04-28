c4c122d1df4286a2b82f54c63739bb04c8474ff4
Haochen Li <haochenl@princeton.edu>
2017-04-28 11:22:25 -0400

    cas

--- myproject/urls.py
+++ myproject/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/myapp/welcome/', permanent=False)),
     url(r'login/$', django_cas_ng.views.login,name='login'),
     url(r'logout/$', django_cas_ng.views.logout,name='logout'),
-    url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
