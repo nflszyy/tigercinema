7e29683ce93e3b249cb0a8ff8ed56278f33cb265
Haochen Li <haochenl@princeton.edu>
2017-04-29 13:25:04 -0400

    cas

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['*']
 
 CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend', 'django_cas_ng.backends.CASBackend',)
-
+CAS_REDIRECT_URL = 'https://pure-temple-10026.herokuapp.com/myapp/welcome/'
 
 # Application definition
 
