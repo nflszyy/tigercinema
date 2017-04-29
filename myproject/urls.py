63b15b4f6924c07a4cf54ec071fb1a27c728aac1
Haochen Li <haochenl@princeton.edu>
2017-04-29 14:09:50 -0400

    cas

--- myproject/urls.py
+++ myproject/urls.py
@@ -10,13 +10,10 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^myapp/', include('myproject.myapp.urls')),
     url(r'^$', RedirectView.as_view(url='/myapp/welcome/', permanent=False)),
-<<<<<<< HEAD
     url(r'accounts/login/$', django_cas_ng.views.login,name='cas_ng_login'),
     url(r'accounts/logout/$', django_cas_ng.views.logout,name='cas_ng_logout'),
     url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback'),
-=======
     url(r'accounts/login/$', django_cas_ng.views.login,name='login'),
     url(r'accounts/logout/$', django_cas_ng.views.logout,name='logout'),
     url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
->>>>>>> 7a80867b79668ce16c854e1657f05276fe2c6965
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
