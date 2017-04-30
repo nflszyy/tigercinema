5a47545b9461d8c0f020183e7ee4fdeec54efa81
Haochen Li <haochenl@princeton.edu>
2017-04-29 22:05:45 -0400

    ratings

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -19,4 +19,4 @@ class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d', validators=[validate_mime_type])
     ratings = GenericRelation(Rating, related_query_name='documents')
 
-Document.objects.filter(ratings__isnull=False).order_by('ratings__average')
+#Document.objects.filter(ratings__isnull=False).order_by('ratings__average')
