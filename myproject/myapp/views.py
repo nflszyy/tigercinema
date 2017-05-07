5592dbde624de1b9ba164895f14fd730b7f57174
Yuyan Zhao <yuyanz@princeton.edu>
2017-05-06 22:45:57 -0400

    reverse

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -21,7 +21,7 @@ from star_ratings.models import Rating
 
 movie_count=1
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def homepage(request):
     user=request.user
     netid=user.username
@@ -36,7 +36,7 @@ def homepage(request):
         {'documents': documents, 'rateddocuments': rateddocuments, 'netid':netid}
     )
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def feedback(request):
     user=request.user
     netid=user.username
@@ -46,7 +46,7 @@ def feedback(request):
 def welcome(request):
     return render(request, 'myapp/welcome.html')
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def uploadform(request):
     user=request.user
     netid=user.username
@@ -88,7 +88,7 @@ def uploadform(request):
         { 'form':form, 'netid':netid}
         )
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def documentary(request):
  # Load documents for the list page
     documents = Document.objects.filter(choice__exact='2')
@@ -102,7 +102,7 @@ def documentary(request):
         {'documents': documents,'netid':netid}
     )
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def narrative(request):
  # Load documents for the list page
     documents = Document.objects.filter(choice__exact='1')
@@ -118,7 +118,7 @@ def narrative(request):
 
 
 # http://stackoverflow.com/questions/20205137/how-to-delete-files-in-django
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def delete(request):
     if request.method != 'POST':
         raise Http404
@@ -128,7 +128,7 @@ def delete(request):
     docToDel.delete()
     return HttpResponseRedirect('/myapp/homepage/')
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def search(request):
 
     if request.method == 'GET':
@@ -152,7 +152,7 @@ def search(request):
             )
         else: return HttpResponseRedirect('/myapp/homepage/')
 
-@login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
+# @login_required(login_url='/accounts/login/',redirect_field_name='/myapp/homepage/')
 def play(request, user_id):
     documents = Document.objects.all()
     user=request.user
