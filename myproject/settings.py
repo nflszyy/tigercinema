b7fe2a903d6a8ccce6c0863cc45c374085d22fce
Haochen Li <haochenl@princeton.edu>
2017-05-09 12:00:14 -0400

    cas

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,14 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-os.environ["SECRET_KEY"]
 CAS_SERVER_URL = 'https://fed.princeton.edu/cas/'
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend', 'django_cas_ng.backends.CASBackend',)
 
